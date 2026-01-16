11d7da3c45ea878339bd85f152b4382082b7da71
yaskov <yaskov@skbkontur.ru>
2026-01-16 19:13:32 +0500

    коммит для деплоя

--- API.Tools/FoodsParserV2/Program.cs
+++ API.Tools/FoodsParserV2/Program.cs
@@ -4,5 +4,3 @@ var parser = new OpenFoodFactsParser();
 // TODO: Add PageSkipping
 var products = await parser.GetRussianProductsAsync(150, 15);
 await FoodsCsvWriter.SaveToCsvAsync(products);
-
-var a = 10;
